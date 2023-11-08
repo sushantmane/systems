fa11bab42ccdada7211a7d282b7a31a3f4eb82af
Sushant Mane <sumane@linkedin.com>
2023-11-07 23:52:29 -0800

    [rust-lang] TRPL Book - Chapter 2: Part 2

--- rust-lang/guessing_game/src/main.rs
+++ rust-lang/guessing_game/src/main.rs
@@ -1,17 +1,33 @@
-use std::io;
+use std::cmp::Ordering;
 use rand::Rng;
+use std::io;
 
 fn main() {
     let secret_number = rand::thread_rng().gen_range(1..=100);
-    println!("The secret number is: {secret_number}");
+    // println!("The secret number is: {secret_number}");
 
     println!("Guess the number!");
 
-    println!("Enter a number:");
-    let mut guess = String::new();
-    io::stdin()
-        .read_line(&mut guess)
-        .expect("Failed to read line");
+    loop {
+        println!("Enter a number:");
+        let mut guess = String::new();
+        io::stdin()
+            .read_line(&mut guess)
+            .expect("Failed to read line");
 
-    println!("You guessed: {guess}");
+        let guess: u32 = match guess.trim().parse() {
+            Ok(num) => num,
+            Err(_) => continue,
+        };
+
+        println!("You guessed: {guess}");
+        match guess.cmp(&secret_number) {
+            Ordering::Less => println!("Too small!"),
+            Ordering::Greater => println!("Too big!"),
+            Ordering::Equal => {
+                println!("You win!");
+                break;
+            }
+        }
+    }
 }
