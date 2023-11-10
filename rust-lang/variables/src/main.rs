4350efe2d7a2c50a40ed6b9ef00da7726f5b6e3b
Sushant Mane <sumane@linkedin.com>
2023-11-09 23:53:13 -0800

    [rust-lang] TRPL Book - Chapter 3: Part 2

--- rust-lang/variables/src/main.rs
+++ rust-lang/variables/src/main.rs
@@ -1,4 +1,5 @@
 fn main() {
+    // Variables
     const NUM_HOURS_IN_DAY: u32 = 24;
     let mut x = 5;
     x = x + 1;
@@ -6,7 +7,7 @@ fn main() {
     let x = 64;
 
     let mut x = 8;
-    let mut y = 18;
+    let y = 18;
     {
         x = x + 1;
         let y = y + 1;
@@ -14,8 +15,39 @@ fn main() {
     }
     println!("The value of : {x} and y: {y}");
 
-    let mut spaces = "  ";
+    let spaces = "  ";
     println!("spaces: {spaces}");
     let spaces = spaces.len();
     println!("spaces: {spaces}");
+
+
+
+    // Data types
+    let mut number: u32 = "43".parse().expect("Not a number!");
+    println!("number: {number}");
+
+    // Integer types
+    let num_8bit: u8 = 118u8;
+    println!("8-bit number: {num_8bit}");
+
+    // signed 8-bit
+    let num_signed_8bit = -128i8;
+    println!("8-bit signed number: {num_signed_8bit}");
+
+    let mut number: u16 = 512u16;
+    println!("unsigned 16 bit number: {number}");
+
+
+    let mut byte = b'A';
+    println!("byte: {byte}");
+
+
+    // Compound Types
+    // tuple
+    let tup: (i32, f64, u8) = (500, 6.4, 1);
+    let (x, y, z) = tup;
+    println!("tuple: {x}, {y}, and {z}");
+
+    let first_val = tup.0;
+    println!("tuple: {first_val}");
 }
