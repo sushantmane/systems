8989ff878057293519abfdf50152768f75a9bdb0
Sushant Mane <sumane@linkedin.com>
2023-11-10 23:51:32 -0800

    [rust-lang] TRPL Book - Chapter 3: Part 4

--- rust-lang/functions/src/main.rs
+++ rust-lang/functions/src/main.rs
@@ -2,6 +2,51 @@
 use std::thread;
 
 fn main() {
+    // learn_functions();
+    // learn_control_flow();
+    learn_loops();
+}
+
+// repetition with loops
+fn learn_loops() {
+// 1) loop
+    let mut idx = 0;
+    loop {
+        println!("Idx: {idx}");
+        if idx % 2 == 0 {
+            idx += 5;
+            continue;
+        }
+
+        idx = idx + 1;
+        if idx >= 100 {
+            break
+        }
+    }
+
+}
+
+
+
+// control flow
+fn learn_control_flow() {
+    let number:i128 = std::i128::MIN;
+    if number > 0 {
+        println!("{number} is a positive integer");
+    } else if number == 0 {
+        println!("{number} is zero");
+    } else {
+        println!("{number} is a negative integer");
+    }
+
+    // if in let
+    let condition:bool = true;
+    let th = if condition {9} else { 6 };
+    println!("Threshold: {th}");
+}
+
+
+fn learn_functions() {
     let thread_id = thread::current().id();
     println!("Hello, world! - thread_id: {:?}", thread_id);
 
@@ -22,6 +67,7 @@ fn main() {
 
 
     println!("function with return value: {}", fun_with_return_val());
+
 }
 
 
