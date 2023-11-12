b53911fca14c3525ecfcf93534a4620af3f3a952
Sushant Mane <sumane@linkedin.com>
2023-11-11 23:54:34 -0800

    [rust-lang] TRPL Book - Chapter 4: Part 2

--- network/readme.md
+++ network/readme.md
@@ -23,3 +23,24 @@ To capture network packets on a specific port (e.g., port 80) and save them to a
 ```bash
 $ sudo tcpdump port 80 -w http.pcap
 ```
+
+## View DNS Trace for a Domain
+
+To trace the DNS (Domain Name System) resolution process for a specific domain (e.g., linkedin.com), use the following command:
+
+```bash
+$ dig +trace linkedin.com
+```
+
+This command provides a detailed view of the DNS resolution path, showing each step in the process, including the authoritative name servers involved in resolving the domain.
+
+
+## Perform Traceroute to a Domain
+
+To trace the network path to a specific domain (e.g., linkedin.com), revealing the routers and transit nodes along the way, use the following command:
+
+```bash
+$ traceroute linkedin.com
+```
+
+This command displays the route that network packets take to reach the destination, helping identify the network infrastructure involved in connecting to the specified domain.
--- rust-lang/chapter4-ownership/src/main.rs
+++ rust-lang/chapter4-ownership/src/main.rs
@@ -2,9 +2,39 @@
 
 fn main() {
     let s1 = give_ownership();
-    let s1 = the_lord_giveth_and_the_lord_taketh_away(s1);
-    take_ownership(s1);
+    let mut s1 = the_lord_giveth_and_the_lord_taketh_away(s1);
+    // take_ownership(s1);
+
+    let len = calculate_length(&mut s1);
+
+    println!("The length of string \"{}\" is {}", s1, len);
+
+    let mut s1 = String::from("hello!");
+
+    let r2 = &s1;
+    let r1 = &mut s1;
+    println!("r1: {}", r1);
+
+
+    let reference_to_nothing = dangle();
+    println!("dangle: {}", reference_to_nothing);
 }
+
+// dangling references
+fn dangle() -> String {
+    let s = String::from("hello");
+    s
+}
+
+
+// references and borrowing
+fn calculate_length(s: &mut String) -> usize {
+    s.push_str(" - append");
+    s.len()
+}
+
+
+
 
 fn give_ownership() -> String {
     let some_string = String::from("The lord giveth and the lord taketh away");
@@ -19,3 +49,6 @@ fn take_ownership(s: String) {
 fn the_lord_giveth_and_the_lord_taketh_away(a_string: String) -> String {
     a_string
 }
+
+
+
