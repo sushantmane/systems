091ac0aadd8f350c6d7cfeb71efbe20e2dec2a48
Sushant Mane <sumane@linkedin.com>
2023-11-13 03:02:26 -0800

    [rust-lang] TRPL Book - Chapter 5: Part 3

--- rust-lang/rectangles/src/main.rs
+++ rust-lang/rectangles/src/main.rs
@@ -3,7 +3,7 @@ fn main() {
     let width1: u32 = 30;
     let height1: u32 = 50;
     println!("AreaR of rectangle with width: {width1} and {height1} is {}",
-             area(width1, height1));
+             area_regular(width1, height1));
 
 
     // approach 2
@@ -22,9 +22,31 @@ fn main() {
 
     println!("AreaS of rectangle with width: {} and {} is {}",
              rect.width, rect.height, area_struct(&rect));
+
+    // approach 4
+    println!("AreaM of rectangle with width: {} and {} is {}",
+             rect.width, rect.height, rect.area());
+
+
+    // other examples
+    let rect2 = Rectangle {
+        width: 10,
+        height: 40
+    };
+
+    let rect3 = Rectangle {
+        width: 60,
+        height: 45
+    };
+
+    println!("rect1: {:?} can hold: {:?} => {}", rect, rect2, rect.can_hold(&rect2));
+    println!("rect1: {:?} can hold: {:?} => {}", rect, rect3, rect.can_hold(&rect3));
+
+    let square1 = Rectangle::square(4);
+    println!("Area square: {:?} is {}", square1, square1.area());
 }
 
-fn area(width: u32, height: u32) -> u32 {
+fn area_regular(width: u32, height: u32) -> u32 {
     width * height
 }
 
@@ -37,6 +59,32 @@ fn area_t(rect: (u32, u32)) -> u32 {
 struct Rectangle {
     width: u32,
     height: u32
+}
+
+impl Rectangle {
+    fn area(&self) -> u32 {
+        self.width * self.height
+    }
+
+    fn width(&self) -> u32 {
+        self.width
+    }
+
+    fn height(&self) -> u32 {
+        self.height
+    }
+
+    fn can_hold(&self, other: &Rectangle) -> bool {
+        self.width > other.width && self.height > other.height
+    }
+
+    // Associated functions
+    fn square(size: u32) -> Self {
+        Self {
+            width: size,
+            height: size
+        }
+    }
 }
 
 fn area_struct(rect: &Rectangle) -> u32 {
