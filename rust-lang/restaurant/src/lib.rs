fe4f7f93269b073dc0efcd9df478a7cd49d585ad
Sushant Mane <sumane@linkedin.com>
2023-11-15 02:12:42 -0800

    [rust-lang] TRPL Book - Chapter 7: Part 2

--- rust-lang/restaurant/src/lib.rs
+++ rust-lang/restaurant/src/lib.rs
@@ -1,6 +1,6 @@
 mod front_of_house {
-    mod hosting {
-        fn add_to_waitlist() {}
+    pub mod hosting {
+        pub fn add_to_waitlist() {}
 
         fn seat_at_table() {}
     }
@@ -12,4 +12,54 @@ mod front_of_house {
 
         fn take_payment() {}
     }
+}
+
+mod back_of_house {
+    fn cook_order() {}
+
+    fn fix_incorrect_order() {
+        cook_order();
+
+        super::deliver_order();
+    }
+
+    pub struct Breakfast {
+        pub toast: String,
+        seasonal_fruit: String,
+    }
+
+    impl Breakfast {
+        pub fn summer(toast: &str) -> Breakfast {
+            Breakfast {
+                toast: String::from(toast),
+                seasonal_fruit: String::from("peaches"),
+            }
+        }
+    }
+
+
+    pub enum Appetizer {
+        Soup,
+        Salad,
+    }
+}
+
+fn deliver_order() {}
+
+
+pub fn eat_at_restaurant() {
+    // // Absolute path
+    // crate::front_of_house::hosting::add_to_waitlist();
+    //
+    // // relative path
+    // front_of_house::hosting::add_to_waitlist();
+
+    let mut meal = back_of_house::Breakfast::summer("Rye");
+    meal.toast = String::from("Wheat");
+    println!("I'd like {} toast please", meal.toast);
+
+    // meal.seasonal_fruit = String::from("berries");
+
+    let order1 = back_of_house::Appetizer::Soup;
+    let order2 = back_of_house::Appetizer::Salad;
 }
