a6f82d753c9c22e3ab3c97ed2eba6f4f03843eec
Sushant Mane <sumane@linkedin.com>
2023-11-15 23:56:04 -0800

    [rust-lang] TRPL Book - Chapter 7: Part 3

--- rust-lang/restaurant/src/lib.rs
+++ rust-lang/restaurant/src/lib.rs
@@ -1,3 +1,5 @@
+use rand::Rng;
+
 mod front_of_house {
     pub mod hosting {
         pub fn add_to_waitlist() {}
@@ -46,20 +48,49 @@ mod back_of_house {
 
 fn deliver_order() {}
 
+pub use crate::front_of_house::hosting;
 
-pub fn eat_at_restaurant() {
-    // // Absolute path
-    // crate::front_of_house::hosting::add_to_waitlist();
-    //
-    // // relative path
-    // front_of_house::hosting::add_to_waitlist();
 
-    let mut meal = back_of_house::Breakfast::summer("Rye");
-    meal.toast = String::from("Wheat");
-    println!("I'd like {} toast please", meal.toast);
+mod customer {
+    use crate::front_of_house::hosting;
 
-    // meal.seasonal_fruit = String::from("berries");
+    pub fn eat_at_restaurant() {
+        // // Absolute path
+        // crate::front_of_house::hosting::add_to_waitlist();
+        //
+        // // relative path
+        // front_of_house::hosting::add_to_waitlist();
 
-    let order1 = back_of_house::Appetizer::Soup;
-    let order2 = back_of_house::Appetizer::Salad;
+        // let mut meal = back_of_house::Breakfast::summer("Rye");
+        // meal.toast = String::from("Wheat");
+        // println!("I'd like {} toast please", meal.toast);
+        //
+        // // meal.seasonal_fruit = String::from("berries");
+        //
+        // let order1 = back_of_house::Appetizer::Soup;
+        // let order2 = back_of_house::Appetizer::Salad;
+
+
+        hosting::add_to_waitlist();
+    }
 }
+
+
+use std::collections::HashMap;
+
+// use std::fmt;
+// use std::io;
+
+// fn function1() -> fmt::Result {}
+//
+// fn function() -> io::Result<()> {}
+
+
+// alternative
+use std::fmt::Result;
+use std::io::Result as IoResult;
+// fn function1() -> Result {}
+//
+// fn function() -> IoResult<()> {}
+
+
