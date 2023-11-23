486245674b068f374200863fdd8a82c2599b0346
Sushant Mane <sumane@linkedin.com>
2023-11-22 23:50:20 -0800

    [rust-lang] TRPL Book - Chapter 8: Part 4.4

---
+++ rust-lang/hash_map/src/assign1.rs
@@ -0,0 +1,18 @@
+use std::fmt::format;
+
+pub fn convert_to_pig_latin(in_str: &str) {
+    let mut res = String::new();
+    for word in in_str.split_whitespace() {
+        if word.starts_with("a")
+            || word.starts_with("e")
+            || word.starts_with("i")
+            || word.starts_with("o")
+            || word.starts_with("u") {
+            res.push_str(&format!("{word}-hay"));
+        } else {
+            res.push_str(&format!("{}-{}ay", &word[1..], &word[0..1]));
+        }
+        res.push_str(" ");
+    }
+    println!("{res}");
+}
--- rust-lang/hash_map/src/main.rs
+++ rust-lang/hash_map/src/main.rs
@@ -5,10 +5,16 @@ use std::collections::HashMap;
 mod learn;
 mod assign;
 
+mod assign1;
+
 fn main() {
 
     // learn::learn_hashmap();
 
-    let mut in_vec = vec![5, 3, 1, 1, 3, 3, 2, 2, 3, 6];
-    assign::get_median_and_mode(&mut in_vec);
+    // let mut in_vec = vec![5, 3, 1, 1, 3, 3, 2, 2, 3, 6];
+    // assign::get_median_and_mode(&mut in_vec);
+
+
+    let in_str = String::from("first apple orange juice");
+    assign1::convert_to_pig_latin(&in_str);
 }
