3074ab3263e5dcde2c21373f8c44fcac8d6b6d26
Sushant Mane <sumane@linkedin.com>
2023-11-22 23:08:06 -0800

    [rust-lang] TRPL Book - Chapter 8: Part 4.3

--- rust-lang/hash_map/src/assign.rs
+++ rust-lang/hash_map/src/assign.rs
@@ -1,5 +1,22 @@
 use std::collections::HashMap;
-pub fn get_median_and_mode(lst: &Vec<i32>) {
+pub fn get_median_and_mode(lst: &mut Vec<i32>) {
+    lst.sort();
+
+    println!("Sorted array: {:?}", lst);
+    println!("Mode: {} Median: {}", get_mode(&lst), get_median(&lst));
+}
+
+fn get_median(lst: &Vec<i32>) -> i32 {
+    if lst.len() == 0 {
+        return -1;
+    }
+
+    let mid = lst.len() / 2;
+    lst[mid]
+}
+
+
+fn get_mode(lst: &Vec<i32>) -> i32 {
     let mut frq = HashMap::new();
     let mut mode:Option<i32> = None;
     let mut mode_frq = 0;
@@ -18,9 +35,10 @@ pub fn get_median_and_mode(lst: &Vec<i32>) {
 
     println!("Mode is {}. It appears {} times", mode.unwrap(), mode_frq);
 
-    for (k, v) in &frq {
-        println!("{k} - {v}");
-    }
+    // for (k, v) in &frq {
+    //     println!("{k} - {v}");
+    // }
 
-
+    // return mode val
+    mode.unwrap()
 }
--- rust-lang/hash_map/src/main.rs
+++ rust-lang/hash_map/src/main.rs
@@ -9,6 +9,6 @@ fn main() {
 
     // learn::learn_hashmap();
 
-    let in_vec = vec![5, 3, 1, 1, 3, 3, 2, 2, 3, 6];
-    assign::get_median_and_mode(&in_vec);
+    let mut in_vec = vec![5, 3, 1, 1, 3, 3, 2, 2, 3, 6];
+    assign::get_median_and_mode(&mut in_vec);
 }
