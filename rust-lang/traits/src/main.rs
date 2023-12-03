97b2d73c845ca60003b7547f48e7cfedf8f04cba
Sushant Mane <sumane@linkedin.com>
2023-12-02 23:45:03 -0800

    [rust-lang] TRPL Book - Chapter 10: Part 3.0

---
+++ rust-lang/traits/src/main.rs
@@ -0,0 +1,18 @@
+// Trait
+
+use crate::lib::{Summary, Tweet};
+
+mod lib;
+
+fn main() {
+
+    let tweet = Tweet {
+        username: String::from("horse_ebooks"),
+        content: String::from("of course, as you probably already know, people",
+        ),
+        reply: false,
+        retweet: false,
+    };
+
+    println!("1 new tweet: {}", tweet.summarize());
+}
